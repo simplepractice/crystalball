ad2ecea55fa3e49009a0aed05f6c34e94ed41709
Jaimerson Araújo <jaimerson@toptal.com>
2018-06-07 20:34:14 -0300

    Add feature specs for filtering behavior of rspec runner
    Also add case for spec directories.

---
+++ features/fixtures/simple_app/prediction_builder.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Sample prediction builder
+class PredictionBuilder < ::Crystalball::RSpec::PredictionBuilder
+  private
+
+  def predictor
+    super do |p|
+      p.use Crystalball::Predictor::ModifiedExecutionPaths.new
+      p.use Crystalball::Predictor::ModifiedSpecs.new
+      p.use ::Crystalball::Predictor::AssociatedSpecs
+        .new(from: /(other_important_class)\.rb/, to: './spec/class2_spec.rb')
+      p.use ::Crystalball::Predictor::AssociatedSpecs
+        .new(from: /(important).*\.rb/, to: './spec/important_dir/')
+      p.use ::Crystalball::Predictor::AssociatedSpecs
+        .new(from: /(class2).*\.rb/, to: './spec/important_dir/important_spec.rb')
+    end
+  end
+end
--- features/rspec_runner_spec.rb
+++ features/rspec_runner_spec.rb
@@ -7,6 +7,8 @@ describe 'RSpec runner' do
     Dir.chdir(root) { `bundle exec crystalball 2>&1`.strip }
   end
   include_context 'simple git repository'
+  let(:important_class_path) { root.join('lib/important_class.rb') }
+  let(:other_important_class_path) { root.join('lib/other_important_class.rb') }
 
   it 'predicts examples' do
     change class1_path
@@ -20,5 +22,35 @@ describe 'RSpec runner' do
     is_expected.to match(/Prediction size \d+ is over the limit \(1\)/)
       .and match(/Prediction is pruned to fit the limit!/)
       .and match(/1 example, 0 failures/)
+  end
+
+  context 'when file, spec id, and directory are predicted' do
+    before do
+      ENV['CRYSTALBALL_EXAMPLES_LIMIT'] = '20'
+    end
+
+    after do
+      ENV['CRYSTALBALL_EXAMPLES_LIMIT'] = nil
+    end
+
+    it 'removes the filters' do
+      change important_class_path
+      change other_important_class_path
+      change class1_path
+
+      is_expected.to match(%r{Prediction:\ \./spec/class2_spec.rb ./spec/important_dir/})
+      is_expected.not_to match(%r{\./spec/class2_spec.rb\[1:1:1\]})
+    end
+
+    context 'when the files are contained in the directories' do
+      it 'only predicts the directory' do
+        change other_important_class_path
+        change important_class_path
+        change class2_path
+
+        is_expected.to match(%r{Prediction:.*\./spec/important_dir/})
+        is_expected.not_to match(%r{\./spec/important_dir/important_spec\.rb})
+      end
+    end
   end
 end
